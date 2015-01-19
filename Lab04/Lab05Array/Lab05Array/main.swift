c414dcb4d1e367d27568f566eb1c3edd6ef4651c
xuguoqing <xuguohun@me.com>
2015-01-19 23:07:09 +0800

    Lab05_Array

---
+++ Lab04/Lab05Array/Lab05Array/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lab05Array
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
---
+++ Lab05Array/Lab05Array/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Lab05Array
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+var arr = ["hhah", 12, 0.1111, "hahah"]
+println(arr)
+
+//指定空数组
+var arr1 = []
+//指定特定类型的数组
+var arr2 = [String]()
+
