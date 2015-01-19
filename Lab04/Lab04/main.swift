be56a4b762168e25fefbbc25ec456b07b48c59e5
xuguoqing <xuguohun@me.com>
2015-01-19 22:59:53 +0800

    Lab04 StringAppend

---
+++ Lab04/Lab04/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Lab04
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+var i = 199
+var str = "Hello "
+str = str + "World ";
+str = "\(str) ** \(100) \(i)"
+
+println(str)
