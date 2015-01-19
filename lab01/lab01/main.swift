1be7c651be6d1632e160b49d3433b1f1afe0830c
xuguoqing <xuguohun@me.com>
2015-01-19 22:49:12 +0800

    helloWorld

---
+++ lab01/lab01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lab01
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, Wish!")
+
