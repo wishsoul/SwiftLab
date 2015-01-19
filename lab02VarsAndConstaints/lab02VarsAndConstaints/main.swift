e209ddfadcce02c262a9fb9963b0de4c4b0adfcd
xuguoqing <xuguohun@me.com>
2015-01-19 22:53:09 +0800

    Lab02 VarsAndConstaints

---
+++ lab02VarsAndConstaints/lab02VarsAndConstaints/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  lab02VarsAndConstaints
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+var a = 1
+var b = 2
+let c = a + b
+println(c)
+
+
