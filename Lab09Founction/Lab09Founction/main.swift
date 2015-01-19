48b332d194e0f9d2c058022674e4ceff9b03a148
xuguoqing <xuguohun@me.com>
2015-01-19 23:37:05 +0800

    Lab09_Founction

---
+++ Lab09Founction/Lab09Founction/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Lab09Founction
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+func haha(haha:String)
+{
+    println(haha)
+}
+
+func qipa()->(String, Int)
+{
+    return ("hahhaw",2)
+}
+
+haha("a hhahah")
+
+let (a, b) = qipa()
+
+println("\(a)  \(b)")
+
