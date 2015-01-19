53329169ba3192df3e002acf9bfa1b6da376fa05
xuguoqing <xuguohun@me.com>
2015-01-19 22:56:41 +0800

    Lab03Types

---
+++ lab03Types/lab03Types/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  lab03Types
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+var str = "hello "
+var s:String = "World "
+var i:Int = 100
+var words:String = "HAHA"
+
+println(s)
