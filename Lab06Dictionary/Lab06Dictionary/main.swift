07dbce80eacf0d3e984426b58634bcec34f0c6d1
xuguoqing <xuguohun@me.com>
2015-01-19 23:09:45 +0800

    Lab06_Dictionary

---
+++ Lab06Dictionary/Lab06Dictionary/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Lab06Dictionary
+//
+//  Created by wishsoul on 15/1/19.
+//  Copyright (c) 2015年 codoon_qing. All rights reserved.
+//
+
+import Foundation
+
+var dict = ["name":"wish" , "age":"15"]
+dict["sex"] = "male"
+println(dict)
+println(dict["name"])
+
